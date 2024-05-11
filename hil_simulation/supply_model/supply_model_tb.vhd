834dff0f2e776d5a7e86cfa1d5fd39c66e43d212
Jari Honkanen <jaarhonka@yahoo.com>
2024-05-11 13:06:20 +0300

    test reordering operations

--- hil_simulation/supply_model/supply_model_tb.vhd
+++ hil_simulation/supply_model/supply_model_tb.vhd
@@ -82,32 +82,33 @@ begin
         return pfc_record
         is
             variable retval : pfc_record := self;
-            variable sum : real_vector(0 to 7);
-            variable mult_add : real_vector(0 to 7);
-            variable mult : real_vector(0 to 9);
+            variable sum      : real_vector (0 to 5);
+            variable mult_add : real_vector (0 to 3);
+            variable mult     : real_vector (0 to 9);
         begin
 
-            sum(1) := self.lc1.current - self.lc2.current;
-            sum(3) := self.lc2.current - self.i3;
-            sum(0) := input_voltage - self.lc1.voltage;
-            sum(2) := self.lc1.voltage - self.lc2.voltage;
-            mult(0) := - r_l*self.lc1.current;
-            mult(1) := r_load * self.lc2.current;
-            mult(2) := - r_l*self.lc2.current;
-            mult(3) := r_load * self.i3;
             mult_add(0) := self.lc2.voltage - self.dc_link*duty;
             mult_add(1) := duty*self.i3 - load_current;
+            sum(1)      := self.lc1.current - self.lc2.current;
+            sum(3)      := self.lc2.current - self.i3;
+            sum(0)      := input_voltage - self.lc1.voltage;
+            sum(2)      := self.lc1.voltage - self.lc2.voltage;
+            mult(0)     := -r_l*self.lc1.current;
+            mult(1)     := r_load * self.lc2.current;
 
-            mult(4) := (sum(1))*c_gain;
-            mult(5) := (sum(3))*c_gain;
-            mult(6) := sum(0)*l_gain;
-            mult(7) := sum(2) * l_gain;
-            sum(4) := mult(0) + mult(1);
-            sum(5) := mult(2) + mult(3);
+            mult(2)     := -r_l*self.lc2.current;
+            mult(3)     := r_load * self.i3;
+            mult(8)     := mult_add(0)*pri_l_gain;
+            mult(9)     := mult_add(1)*dc_link_gain;
+            mult(4)     := (sum(1))*c_gain;
+            mult(5)     := (sum(3))*c_gain;
+            mult(6)     := sum(0)*l_gain;
+            mult(7)     := sum(2) * l_gain;
+            sum(4)      := mult(0) + mult(1);
+            sum(5)      := mult(2) + mult(3);
+
             mult_add(2) := mult(6) + (sum(4)) * l_gain;
             mult_add(3) := mult(7) + (sum(5)) * l_gain;
-            mult(8) := mult_add(0)*pri_l_gain;
-            mult(9) := mult_add(1)*dc_link_gain;
 
             retval.lc1.current := mult_add(2);
             retval.lc1.voltage := mult(4);
