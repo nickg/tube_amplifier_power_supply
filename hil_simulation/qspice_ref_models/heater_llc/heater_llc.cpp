07552f5c143b9d1ef64fe38fd933ee38438ecee3
Jari Honkanen <jaarhonka@yahoo.com>
2024-05-24 07:53:57 +0300

    fix llc modulator

--- hil_simulation/qspice_ref_models/heater_llc/heater_llc.cpp
+++ hil_simulation/qspice_ref_models/heater_llc/heater_llc.cpp
@@ -5,18 +5,23 @@
 #include "../../cpp_sources/carrier_generation/carrier_generation.hpp"
 #include "../../cpp_sources/feedback_control/pi_control/pi_control.hpp"
 
-const double
-    gate_hi_voltage = 6.0   ,
-    gate_lo_voltage = -3.3  ,
-    deadtime        = 50e-9 ,
-    Ts              = 1.0/110.0e3 ,
+double
+    gate_hi_voltage = 6.0         ,
+    gate_lo_voltage = -3.3        ,
+    dt              = 150e-9      ,
+    Ts              = 1.0/150.0e3 ,
     duty            = 0.5;
 
-Modulator modulator1(Ts, duty, gate_hi_voltage, gate_lo_voltage, deadtime);
-Modulator modulator2(Ts, duty, gate_hi_voltage, gate_lo_voltage, deadtime);
+Modulator modulator1(Ts, duty, gate_hi_voltage, gate_lo_voltage, dt);
+Modulator modulator2(Ts, duty, gate_hi_voltage, gate_lo_voltage, dt);
 
-const double 
-    vkp = 0.014,
+DeadtimeController deadtime(gate_hi_voltage, gate_lo_voltage, dt);
+
+double 
+    pwm = 0     ,
+    t0  = 0     ,
+    t1  = 0     ,
+    vkp = 0.014 ,
     vki = 0.01;
 
 pi_control voltage_control(vkp, vki, 1.0);
@@ -56,7 +61,7 @@ extern "C" __declspec(dllexport) void heater_llc(void **opaque, double t, union 
    double  vdc      = data[0].d; // input
    double &gate1    = data[1].d; // output
    double &gate2    = data[2].d; // output
-   double &carrier2 = data[3].d; // output
+   double &carrier  = data[3].d; // output
    double &vin      = data[4].d; // output
    double &carrier1 = data[5].d; // output
    double &gate3    = data[6].d; // output
@@ -74,27 +79,39 @@ extern "C" __declspec(dllexport) void heater_llc(void **opaque, double t, union 
 
     }
     if (modulator1.synchronous_sample_called(t)) {
-        modulator1.set_frequency(175e3 - (70e3*vout));
+        /* modulator1.set_frequency(175e3 - (70e3*vout)); */
         /* modulator1.set_phase((175e3 - (70e3*vout))*0.5); */
     }
 
+    if (t > t0 + Ts/2){
+        t0 = t;
+        Ts = 1.0/(200e3 - (70e3*vout));
+
+        if (pwm == 0) {
+            pwm = 1;
+        }
+        else {
+            pwm = 0;
+        }
+    }
+    deadtime.update(t, pwm);
 
    vin = 400.0;
    modulator1.update(t);
    modulator2.update(t);
    carrier1 = modulator1.get_carrier();
-   carrier2 = modulator2.get_carrier();
-   gate1 = modulator1.getPWM();
-   gate2 = modulator1.getPWMLo();
+   /* carrier2 = modulator2.get_carrier(); */
+   gate1 = deadtime.getPWM_hi();
+   gate2 = deadtime.getPWM_lo();
    gate3 = gate_lo_voltage;
    gate4 = gate_lo_voltage;
    iload = 0.0;
 
    // excitation 
-   iload = -8.0;
-   /* if (t > 2.5e-3) iload = -8.5; */
-   /* if (t > 4.5e-3) iload = 1.0; */
-   /* if (t > 5.5e-3) iload = -5.0; */
-   /* if (t > 7.5e-3) iload = 5.0; */
+   iload = 0; 
+   if (t > 2.5e-3) iload = -8.5;
+   if (t > 4.5e-3) iload = -12.0;
+   if (t > 5.5e-3) iload = -3.0;
+   if (t > 7.5e-3) iload = -1.0;
    /* if (t > 9.5e-3) iload = -1.0; */
 }
